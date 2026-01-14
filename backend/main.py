a903b3f60d959f3917d67d5aa568a1e331e57c76
ldaeasytech <lda.easytech@gmail.com>
2026-01-14 14:33:48 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from thermo import water_props
 
-app = FastAPI(title="Steam Property API")
+app = FastAPI()
+
+# ⚠️ Add CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://steam.easytechcalculators.com"],  # your frontend domain
+    allow_credentials=True,
+    allow_methods=["*"],  # GET, POST, etc.
+    allow_headers=["*"],  # allows all headers
+)
 
 @app.get("/api/water")
 def calculate(T: float, P: float):
-    """
-    T: °C
-    P: MPa
-    """
     return water_props(T, P)
